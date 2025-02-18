17aeed86d9dffdfb79bdb300a7f444102833931f
sebasdex <sebasdc9@hotmail.com>
2025-02-18 17:59:54 -0600

    Add responsive image handling and resize event listener in Deserts component

--- src/components/Deserts.tsx
+++ src/components/Deserts.tsx
@@ -1,10 +1,17 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import data from "../data/data.json";
 import ButtonAddCartHover from "./ButtonAddCartHover";
 
 function Deserts() {
   const [isHover, setIsHover] = useState(false);
   const [hoverIndex, setHoverIndex] = useState<number | null>(null);
+  const [currentSizeScreen, setCurrentSizeScreen] = useState(window.innerWidth);
+
+  useEffect(() => {
+    window.addEventListener("resize", () => {
+      setCurrentSizeScreen(window.innerWidth);
+    });
+  }, []);
   const handleMouseEnter = (index: number) => {
     setHoverIndex(index);
     setIsHover(true);
@@ -15,36 +22,35 @@ function Deserts() {
       <h1 className="title">Desserts</h1>
       <section className="items-container">
         {data.map((item, index) => (
-          <>
-            <article className="item" key={index}>
-              <img
-                className={isHover && hoverIndex === index ? "image-hover" : ""}
-                src={item.image.mobile}
-                alt={item.name.split(" ").join("-").toLowerCase()}
-              />
+          <article className="item" key={index}>
+            <img
+              className={isHover && hoverIndex === index ? "image-hover" : ""}
+              src={currentSizeScreen >= 800 ? item.image.desktop : item.image.mobile}
+              alt={item.name.split(" ").join("-").toLowerCase()}
+            />
+            <div className="item-info-container">
               <span className="item-category">{item.category}</span>
               <span className="item-name">{item.name}</span>
               <span className="item-price">${item.price.toFixed(2)}</span>
+            </div>
 
-              {isHover && hoverIndex === index ? (
-                <ButtonAddCartHover setIsHover={setIsHover} setHoverIndex={setHoverIndex} />
-              ) : (
-                <>
-                  <button
-                    className="button-add-to-cart"
-                    onMouseEnter={() => handleMouseEnter(index)}
-                  >
-                    <img
-                      src="./assets/images/icon-add-to-cart.svg"
-                      alt="add-to-cart"
-                    />
-                    Add to Cart
-                  </button>
-                </>
-              )}
-            </article>
-
-          </>
+            {isHover && hoverIndex === index ? (
+              <ButtonAddCartHover setIsHover={setIsHover} setHoverIndex={setHoverIndex} />
+            ) : (
+              <>
+                <button
+                  className="button-add-to-cart"
+                  onMouseEnter={() => handleMouseEnter(index)}
+                >
+                  <img
+                    src="./assets/images/icon-add-to-cart.svg"
+                    alt="add-to-cart"
+                  />
+                  Add to Cart
+                </button>
+              </>
+            )}
+          </article>
         ))}
       </section>
     </section>
