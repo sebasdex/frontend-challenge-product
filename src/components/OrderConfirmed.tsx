1fc89a161b4943496984514c8e57e912b4d60a04
sebasdex <sebasdc9@hotmail.com>
2025-02-23 20:33:07 -0600

    feat: Enhance OrderConfirmed component to display ordered items and total price dynamically

--- src/components/OrderConfirmed.tsx
+++ src/components/OrderConfirmed.tsx
@@ -1,6 +1,6 @@
 import { useStore } from "./../hooks/useStore";
 function OrderConfirmed() {
-    const { setOrderCard, reset } = useStore();
+    const { setOrderCard, reset, cartItems } = useStore();
     const handleNewOrder = () => {
         setOrderCard(false);
         reset();
@@ -12,9 +12,26 @@ function OrderConfirmed() {
                 <h1>Order Confirmed</h1>
                 <p>We hope you enjoy your food!</p>
                 <div className="order-items-container">
+                    {cartItems.map((item, index) => (
+                        <div className="order-item" key={index}>
+                            <div className="order-item-div">
+                                <span className="order-item-icon-span">
+                                    <img src={item.image.thumbnail} alt={item.name} />
+                                </span>
+                                <div className="order-item-info">
+                                    <h2>{item.name}</h2>
+                                    <div className="order-item-div">
+                                        <p>{item.quantity}x</p>
+                                        <p>@{item.price.toFixed(2)}</p>
+                                    </div>
+                                </div>
+                            </div>
+                            <span className="order-item-total">${(item.price * item.quantity).toFixed(2)}</span>
+                        </div>
+                    ))}
                     <div className="order-price">
                         <span className="order-price-text">Order total</span>
-                        <span className="order-price-value">$100.00</span>
+                        <span className="order-price-value">${cartItems.reduce((acc, item) => acc + item.price * item.quantity, 0).toFixed(2)}</span>
                     </div>
                 </div>
                 <button className="order-button" onClick={handleNewOrder}>Start New Order</button>
